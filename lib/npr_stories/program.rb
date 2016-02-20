c7ca825a912d6b29dba6be54e7139cbadbea0fd0
Tracy <tracyctran@gmail.com>
2016-02-19 22:55:27 -0800

    Corrected show/program/story terminology.

--- lib/npr_stories/program.rb
+++ lib/npr_stories/program.rb
@@ -1,9 +1,10 @@
 class NprStories::Program
+  #think of a better class name. feel like program is kind of confusing. radio show?
 
   def self.all
 
   #this will list all NPR programs
-    puts "All the stories are here."
+    puts "All the programs are here."
   end
 
 end
