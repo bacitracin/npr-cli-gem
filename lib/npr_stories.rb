29343d59a5c612f35acea5e64af81661369f025d
Tracy <tracetrace@gmail.com>
2016-02-23 19:05:41 +0000

    added in array_of_story_hashes to save each story

--- lib/npr_stories.rb
+++ lib/npr_stories.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 require_relative "./npr_stories/version"
 require_relative "./npr_stories/cli"
 
-class Story
+class StoryScraper
 
 # NPR story search that limits results to 5 stories. Using this NPR account's key: tracetrace@gmail.com
 # Results come back as XML, other options including JSON available. More info here http://www.npr.org/api/inputReference.php
@@ -23,19 +23,32 @@ WAIT_WAIT = '35'
 WEEKEND_EDITION_SATURDAY = '7'
 WEEKEND_EDITION_SUNDAY = '10'
 
-def pull_stories(program)
-  api_query = BASE_URL + program
-  @doc = Nokogiri::XML(open(api_query))
-  @set_of_stories = @doc.xpath('//story')
+  def pull_stories(program)
+    array_of_story_hashes = []
+    api_query = BASE_URL + program
+    @doc = Nokogiri::XML(open(api_query))
+    @stories = @doc.xpath('//story') # @stories is an array of 5 Nokogiri story objects
 
-  @set_of_stories.each do |story|
-    @program_title = @doc.css('story program')[0].text
-    @story_title =@doc.css('story title')[0].text
-    @story_date = @doc.css('story storyDate')[0].text
-    @teaser = @doc.css('story teaser')[0].text
-    @story_url = @doc.css('story link')[0].text
-    @topic = @doc.css('story slug')[0].text
+    #iterates over the stories and ends with an array of story hashes
+    @stories.each do |story|
+      story_title =@doc.css('story title').text
+      program_title = @doc.css('story program').text
+      story_date = @doc.css('story storyDate').text
+      teaser = @doc.css('story teaser').text
+      story_url = @doc.css('story link').text
+      topic = @doc.css('story slug').text
+      array_of_story_hashes << {
+        :story_title => story_title,
+        :program_title => program_title,
+        :story_date => story_date,
+        :teaser => teaser,
+        :story_url => story_url,
+        :topic => topic
+        }
+    end
+    array_of_story_hashes
   end
-end
 
+end
+end
 
